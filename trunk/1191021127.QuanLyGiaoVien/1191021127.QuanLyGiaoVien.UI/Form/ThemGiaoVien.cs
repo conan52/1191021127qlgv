4b7a9e64a4e58e04d141fca9df96c813ab337b43
trandainghia07us@gmail.com <trandainghia07us@gmail.com@cb7c9b2f-2fa3-8d48-476b-5a5e7658f016>
2012-12-02 12:11:48 +0000

    xoa data source

--- trunk/1191021127.QuanLyGiaoVien/1191021127.QuanLyGiaoVien.UI/Form/ThemGiaoVien.cs
+++ trunk/1191021127.QuanLyGiaoVien/1191021127.QuanLyGiaoVien.UI/Form/ThemGiaoVien.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Data.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Collections;
@@ -13,63 +14,21 @@ namespace _1191021127.QuanLyGiaoVien.UI.Common
 {
     public partial class ThemGiaoVien : Form
     {
+        DBStoreDataContext db = new DBStoreDataContext();
+
         public ThemGiaoVien()
         {
             InitializeComponent();
         }
-      //  QLGVEntities data = new QLGVEntities();
-       
 
         private void button3_Click(object sender, EventArgs e)
         {
             this.Close();
         }
-
-        private void ThemGiaoVien_Load(object sender, EventArgs e)
-        {
-            // TODO: This line of code loads data into the 'quanLyGiaoVienDataSet1.ChucDanh' table. You can move, or remove it, as needed.
-           // this.chucDanhTableAdapter.Fill(this.quanLyGiaoVienDataSet1.ChucDanh);
-            // TODO: This line of code loads data into the 'quanLyGiaoVienDataSet1.ChucDanh' table. You can move, or remove it, as needed.
-         
-            // TODO: This line of code loads data into the 'quanLyGiaoVienDataSet.BoMon' table. You can move, or remove it, as needed.
-            //this.boMonTableAdapter.Fill(this.quanLyGiaoVienDataSet.BoMon);
-            try
-            {
-                // TODO: This line of code loads data into the 'quanLyGiaoVienDataSet.ChucDanh' table. You can move, or remove it, as needed.
-               
-                //Display List GioiTinh
-              //  ArrayList gt = new ArrayList { "Nam", "Nữ" };
-               // cbGioiTinh.DataSource = gt;
-
-               // chucDanhBindingSource.DataSource = data.ChucDanhs;
-
-               // boMonBindingSource.DataSource = data.BoMons;
-
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-            
-        }
-
-        
-
-        private void cbChucVu_SelectedIndexChanged(object sender, EventArgs e)
-        {
-           // cbChucVu.DataSource = data.ChucDanhs;
-        }
-             
-
-        private void cbBoMon_SelectedIndexChanged(object sender, EventArgs e)
-        {
-           // cbBoMon.DataSource = data.BoMons;
-        }
+       
 
         private void button2_Click(object sender, EventArgs e)
         {
-           DBStoreDataContext  db = new DBStoreDataContext();
             GiaoVien giaoVien = new GiaoVien();
             giaoVien.HoTen = "Trần Đại Nghĩa";
             giaoVien.GioiTinh = "Nam";
@@ -83,6 +42,17 @@ namespace _1191021127.QuanLyGiaoVien.UI.Common
 
             db.GiaoViens.InsertOnSubmit(giaoVien);
             db.SubmitChanges();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ThemGiaoVien_Load(object sender, EventArgs e)
+        {
+            Table<ChucDanh> chucDanhs = db.GetTable<ChucDanh>();
+            cbChucVu.DataSource = chucDanhs;
         }
     }
 }
